dc4fca202e0167edd53a8cb63c32ffaa8edfbe15
Erfanshirani <89613452+Erfanshirani@users.noreply.github.com>
2025-03-25 03:59:16 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# فایل config.py (برای ذخیره اطلاعات حساس)
+TOKEN = "7708335061:AAHwNtnmLUBT0meIHoSYzARgqw2gl-hrONA"  # توکن ربات شما
+CHANNEL_ID = "@fast_Proxy_pm"  # آیدی کانال
+PROXY_API = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http"
